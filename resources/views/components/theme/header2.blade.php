ea5ccbcb87ff32cbe3aaaa45224a21d32c0e13ef
Fahrizaldi <45739897+aldmf26@users.noreply.github.com>
2025-04-24 15:31:45 +0800

    das

--- resources/views/components/theme/header2.blade.php
+++ resources/views/components/theme/header2.blade.php
@@ -1,7 +1,7 @@
 <div class="header-top">
     <div class="container">
         <div class="logo text-center">
-            <a href="dashboard"><img src="{{asset('img/logocash-tf.png')}}" alt="Logo"></a>
+            <a href="dashboard"><img width="200" src="{{asset('img/logocash-tf.png')}}" alt="Logo"></a>
             <h5>CatatDuit</h5>
         </div>
         <div class="header-top-right">
