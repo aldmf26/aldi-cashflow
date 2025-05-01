282a6adba0edb0543fb80d367d81460aeb5e949e
aldimf26 <aldimf26@gmail.com>
2025-05-01 19:56:54 +0800

    update

--- app/Charts/KategoriChart.php
+++ app/Charts/KategoriChart.php
@@ -15,11 +15,18 @@ class KategoriChart
 
     public function build(): \ArielMejiaDev\LarapexCharts\PieChart
     {
+        $wallets = [
+            ['nama' => 'Cash', 'saldo' => 100000],
+            ['nama' => 'Bank BNI', 'saldo' => 500000],
+            ['nama' => 'Bank BRI', 'saldo' => 1000000],
+            ['nama' => 'Bank BCA', 'saldo' => 500000],
+        ];
+        $total = collect($wallets)->sum('saldo');
         return $this->chart->pieChart()
-            ->setTitle('Top 3 scorers of the team.')
-            ->setSubtitle('Season 2021.')
-            ->addData([40, 50, 30])
-            ->setLabels(['Player 7', 'Player 10', 'Player 9'])
+            ->setTitle('Dompet')
+            ->setSubtitle('Total Saldo: Rp. ' . number_format($total, 0, ',', '.'))
+            ->addData(collect($wallets)->pluck('saldo')->toArray())
+            ->setLabels(collect($wallets)->pluck('nama')->toArray())
             ->setFontFamily('Poppins');
 
     }
